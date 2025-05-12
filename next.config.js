c1ff22459f4702ed2a5866aad0ecc5198e5c2054
Seraph <seraph.axl@gmail.com>
2025-05-12 12:49:08 -0600

    Actualizar configuración de Netlify: simplificar comentarios en next.config.js para mejorar la claridad.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuración esencial para Netlify
   output: 'standalone',
-
-  // Configuración para API Routes
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ['stripe'],
     outputFileTracingRoot: path.join(__dirname, '../../')
   },
-
-  // Otras configuraciones
   eslint: {
     ignoreDuringBuilds: true
   },
