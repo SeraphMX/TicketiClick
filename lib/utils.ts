5767fc9b51a3032582455aff95a469c79508fe0d
Seraph <seraph.axl@gmail.com>
2025-05-18 09:45:25 -0600

    Refactor: mejorar estilos en globals.css y optimizar funciones de fecha y HTML en utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,34 +1,36 @@
 import { clsx, type ClassValue } from 'clsx'
+import { decode } from 'he'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-// Función para formatear fechas a formato español
+// Función para formatear fechas a formato español y evitar desfase por zona horaria
 export const formatDate = (dateString: string) => {
+  const [year, month, day] = dateString.split('-').map(Number)
+  // El mes en JS es base 0, por eso restamos 1
+  const date = new Date(year, month - 1, day)
   const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
-  return new Date(dateString).toLocaleDateString('es-ES', options)
+  return date.toLocaleDateString('es-MX', options)
 }
 
-//Funcion para formatear hora a formato español de 12 horas
+// Función para formatear hora a formato local de 12 horas o 24 según la configuración del usuario
 export const formatTime = (timeString: string) => {
   const [hours, minutes] = timeString.split(':').map(Number)
-  const ampm = hours >= 12 ? 'PM' : 'AM'
-  const formattedHours = hours % 12 || 12 // Convertir a formato de 12 horas
-  return `${formattedHours}:${minutes.toString().padStart(2, '0')} ${ampm}`
+  const date = new Date()
+  date.setHours(hours, minutes, 0, 0)
+  return date.toLocaleTimeString(undefined, {
+    hour: '2-digit',
+    minute: '2-digit'
+  })
 }
 
 //Funcion para cpmvertitr html a textp plano
+
 export const convertHtmlToText = (html: string) => {
-  // Reemplazar etiquetas BR con espacios
-  let text = html.replace(/<br\s*\/?>/gi, ' ')
-  // Reemplazar otras etiquetas HTML
-  text = text.replace(/<[^>]+>/g, '')
-  // Decodificar entidades HTML
-  text = text.replace(/&nbsp;/gi, ' ')
-  text = text.replace(/&amp;/gi, '&')
-  text = text.replace(/&lt;/gi, '<')
-  text = text.replace(/&gt;/gi, '>')
-  // Eliminar espacios múltiples y trim
-  return text.replace(/\s+/g, ' ').trim()
+  const withoutTags = html
+    .replace(/<br\s*\/?>/gi, ' ') // o '\n' si quieres saltos de línea
+    .replace(/<[^>]+>/g, '')
+  const decoded = decode(withoutTags)
+  return decoded.replace(/\s+/g, ' ').trim()
 }
