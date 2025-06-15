98699c07d90413e109d369ecd54165738d56757d
Seraph <seraph.axl@gmail.com>
2025-06-15 11:56:37 -0600

    fix: ajustar el estilo del contenedor en la página de verificación de email

--- app/cuenta/reset-password/[token]/page.tsx
+++ app/cuenta/reset-password/[token]/page.tsx
@@ -6,7 +6,7 @@ export default async function VerifyEmail(props: { params: Props }) {
   const { token } = await props.params
   const payload = verifyEmailVerifyToken(token)
   return (
-    <div className='min-h-screen flex items-center p-6'>
+    <div className='min-h-screen flex items-center justify-center p-5'>
       <PasswordResetMailClient email={payload?.email || null} />
     </div>
   )
