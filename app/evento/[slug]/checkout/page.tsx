bef829f725e4f7919e727bc9471520d422b10ac5
Seraph <seraph.axl@gmail.com>
2025-06-30 05:09:13 -0600

    feat: mejorar el diseño de la página de checkout para una mejor presentación de errores y contenido

--- app/evento/[slug]/checkout/page.tsx
+++ app/evento/[slug]/checkout/page.tsx
@@ -13,7 +13,7 @@ export default async function CheckoutPage(props: { params: Props }) {
 
   if (!event) {
     return (
-      <div className='min-h-screen bg-gray-50 py-12'>
+      <div className='min-h-screen flex items-center justify-center p-5 '>
         <div className='max-w-2xl mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='bg-white p-6 rounded-lg shadow-md'>
             <p className='text-center text-gray-600'>Evento no encontrado</p>
@@ -23,5 +23,9 @@ export default async function CheckoutPage(props: { params: Props }) {
     )
   }
 
-  return <CheckoutClient event={event as Event} />
+  return (
+    <div className='min-h-screen flex items-center justify-center p-5 pt-24 pb-10'>
+      <CheckoutClient event={event as Event} />
+    </div>
+  )
 }
