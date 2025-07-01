d542bcfcf3653578fec7d95b563ee25dfda8c6cb
Seraph <seraph.axl@gmail.com>
2025-07-01 14:18:54 -0600

    feat: implementar manejo de escaneo de tickets y mostrar datos en la UI

--- app/check-in/page.tsx
+++ app/check-in/page.tsx
@@ -1,17 +1,32 @@
 'use client'
+import { ticketService } from '@/services/ticketService'
 import { IDetectedBarcode, outline, Scanner } from '@yudiel/react-qr-scanner'
 import { useState } from 'react'
 export default function Page() {
   const [qrData, setQrData] = useState<string | null>(null)
+  const [ticketData, setTicketData] = useState<any>(null)
+
+  const handleScan = async (result: IDetectedBarcode[]) => {
+    console.log(result)
+    if (result && result.length > 0) {
+      setQrData(result[0].rawValue)
+
+      const ticket = await ticketService.getTicketbyCode(result[0].rawValue)
+
+      if (ticket) {
+        console.log('Ticket data:', ticket)
+        setTicketData(ticket)
+        // Aquí puedes manejar los datos del ticket escaneado, por ejemplo, mostrarlo en la UI
+      } else {
+        console.error('No se encontró el ticket con el código escaneado.')
+      }
+    }
+  }
+
   return (
     <div>
       <Scanner
-        onScan={(result: IDetectedBarcode[]) => {
-          console.log(result)
-          if (result) {
-            setQrData(result[0].rawValue)
-          }
-        }}
+        onScan={handleScan}
         sound={true}
         components={{
           torch: true,
@@ -19,12 +34,21 @@ export default function Page() {
           tracker: outline
         }}
       />
-      <p>Scan a QR code</p>
+
       {qrData && (
         <div>
           <p>QR Code Data:</p>
           <pre>{qrData}</pre>
         </div>
+      )}
+
+      <section>Datos del ticket escaneado:</section>
+      {ticketData ? (
+        <div>
+          <pre>{JSON.stringify(ticketData, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>No se ha escaneado ningún ticket aún.</p>
       )}
     </div>
   )
