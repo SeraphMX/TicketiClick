82f511785c8e506fdddfe75df4a0003c646ac2c6
Seraph <seraph.axl@gmail.com>
2025-07-01 12:28:22 -0600

    feat: implementar escaneo de códigos QR y mostrar datos en la página de Check-in

--- app/dashboard/check-in/page.tsx
+++ app/dashboard/check-in/page.tsx
@@ -1,9 +1,25 @@
 'use client'
-import { Scanner } from '@yudiel/react-qr-scanner'
+import { IDetectedBarcode, Scanner } from '@yudiel/react-qr-scanner'
+import { useState } from 'react'
 export default function Page() {
+  const [qrData, setQrData] = useState<string | null>(null)
   return (
     <div>
-      <Scanner onScan={(result) => console.log(result)} />
+      <Scanner
+        onScan={(result: IDetectedBarcode[]) => {
+          console.log(result)
+          if (result) {
+            setQrData(result[0].rawValue)
+          }
+        }}
+      />
+      <p>Scan a QR code</p>
+      {qrData && (
+        <div>
+          <p>QR Code Data:</p>
+          <pre>{qrData}</pre>
+        </div>
+      )}
     </div>
   )
 }
